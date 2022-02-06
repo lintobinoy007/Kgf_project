33790547cc5f17d8baf0a3d533f8ae4499dacfbe
linto binoy <93820158+lintobinoy007@users.noreply.github.com>
2022-02-06 19:26:33 +0530

    Update __main__.py

--- EmikoRobot/__main__.py
+++ EmikoRobot/__main__.py
@@ -82,7 +82,7 @@ def get_readable_time(seconds: int) -> str:
 
 PM_START_TEXT = """
 *Hello {} !*
-✪ I'm an anime-theme management bot [✨](https://telegra.ph/file/11b5922a33de9968cedfe.jpg)
+✪ I'm an KGf  management bot [✨](https://telegra.ph/file/ee90389214bc05743f56a.jpg)
 ────────────────────────
 × *Uptime:* `{}`
 × `{}` *users, across* `{}` *chats.*
